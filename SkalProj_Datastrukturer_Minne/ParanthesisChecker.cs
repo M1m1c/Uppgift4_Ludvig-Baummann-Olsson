becc961fd9014f4c5f7ea45f0618997c05fad54c
ludvi <ludvi@DESKTOP-6U5E7KR>
2020-03-30 16:57:56 +0200

    Refactored CheckParanthesis.

--- SkalProj_Datastrukturer_Minne/ParanthesisChecker.cs
+++ SkalProj_Datastrukturer_Minne/ParanthesisChecker.cs
@@ -6,7 +6,6 @@ namespace SkalProj_Datastrukturer_Minne
 {
     public class ParanthesisChecker
     {
-
         public static void CheckParanthesis()
         {
             Console.Clear();
@@ -18,18 +17,44 @@ namespace SkalProj_Datastrukturer_Minne
                 char nav = input.Length > 0 ? input[0] : 'n';
 
                 if (nav == '0' && input.Length < 2) return;
+               
+                Stack<char> openings = new Stack<char>();             
+                bool correctFormatting = true;
 
-                var brackets = FindBrackets(input);
-                if (brackets.Count == 0)
+                foreach (var item in input)
                 {
-                    Console.WriteLine("Found no opening bracket or found a closing bracket before an opening," +
-                                      " string had incorrect formatting");
-                    continue;
+                    if (correctFormatting == false) { break; }
+
+                    switch (item)
+                    {
+                        case '(':
+                            openings.Push('(');
+                            break;
+                        case '[':
+                            openings.Push('[');
+                            break;
+                        case '{':
+                            openings.Push('{');
+                            break;
+                        case ')':
+                            if (openings.Peek() != '(')
+                            { correctFormatting = false; }
+                            else { openings.Pop(); }
+                            break;
+                        case ']':
+                            if (openings.Peek() != '[')
+                            { correctFormatting = false; }
+                            else { openings.Pop(); }
+                            break;
+                        case '}':
+                            if (openings.Peek() != '{')
+                            { correctFormatting = false; }
+                            else { openings.Pop(); }
+                            break;
+                    }
                 }
 
-                GoThroughBrackets(brackets);
-
-                if (brackets.Count == 0)
+                if (correctFormatting)
                 {
                     Console.WriteLine("Correct formatting, all brackets were enclosed");
                 }
@@ -38,84 +63,6 @@ namespace SkalProj_Datastrukturer_Minne
                     Console.WriteLine("Incorrect formatting, string has one or more unenclosed brackets");
                 }
             }
-        }
-
-        //Returns all brackets in the input string
-        private static List<char> FindBrackets(string input)
-        {
-            List<char> temp = new List<char>();
-            foreach (var item in input)
-            {
-                if (temp.Count == 0 && (item == ')' || item == ']' || item == '}'))
-                {
-                    break;
-                }
-                else if (item == '(' || item == '[' || item == '{' || item == ')' || item == ']' || item == '}')
-                {
-                    temp.Add(item);
-                }
-            }
-            return temp;
-        }
-
-        //Loops through brackets list, restarts from the last index if FoundEnclosedBrackets succeeds
-        private static void GoThroughBrackets(List<char> brackets)
-        {
-            int i = brackets.Count - 1;
-            while (-1 < i)
-            {
-                if (FoundEnclosedBrackets(brackets, i))
-                {
-                    i = brackets.Count - 1;
-                    continue;
-                }
-                i--;
-            }
-        }
-
-        //Finds the last opening to appear in list and calls FoundClosing(), returns true if FoundClosing() Succeeds
-        private static bool FoundEnclosedBrackets(List<char> brackets, int i)
-        {
-            bool retFlag = false;
-            switch (brackets[i])
-            {
-                case '(':
-                    if (FoundClosing(brackets, i, ')'))
-                    {
-                        retFlag = true;
-                    }
-                    break;
-                case '[':
-                    if (FoundClosing(brackets, i, ']'))
-                    {
-                        retFlag = true;
-                    }
-                    break;
-                case '{':
-                    if (FoundClosing(brackets, i, '}'))
-                    {
-                        retFlag = true;
-                    }
-                    break;
-            }
-            return retFlag;
-        }
-
-        //Returns true if the elment behind index "i" contains the wanted symbol,
-        //removes opening and closing brackets from list on succees.
-        private static bool FoundClosing(List<char> brackets, int i, char wantedClosing)
-        {
-            bool retFlag = false;
-            if (i + 1 < brackets.Count)
-            {
-                if (brackets[i+1] == wantedClosing)
-                {
-                    brackets.RemoveAt(i+1);
-                    brackets.RemoveAt(i);
-                    retFlag = true;
-                }
-            }           
-            return retFlag;
         }
     }
 }
